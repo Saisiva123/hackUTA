351d3101c946d2f2976b84d79a97e62c442c50c3
Sai Siva Durga Prasad <mannem.sai99@gmail.com>
2023-10-07 14:53:12 -0500

    intial setup

---
+++ client/maintenance/screens/OnboardingScreen.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+const OnBoardingScreen = () => {
+  return (
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text>boarding Screen</Text>
+    </View>
+  )
+}
+
+export default OnBoardingScreen
---
+++ client/maintenance/screens/RegistrationScreen.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+const RegistrationScreen = () => {
+  return (
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text>Registration Screen</Text>
+    </View>
+  )
+}
+
+export default RegistrationScreen
